0903b00bb0159fa66fc8cc5fb557212269edad4c
sinarah22 <sinarahmannejad@gmail.com>
2019-09-30 09:39:59 -0700

    welcome to Java

---
+++ SecondJava/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+//type psvm and hit tab the line below will appear
+    public static void main(String[] args) {
+        System.out.print("H");
+        System.out.println("ello Death!!");
+    }
+}
