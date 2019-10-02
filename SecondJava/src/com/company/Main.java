691c1264fae92837c3a763d5da7084283d4506ac
sinarah22 <sinarahmannejad@gmail.com>
2019-10-01 19:23:02 -0700

    bunch of new stuff

--- SecondJava/src/com/company/Main.java
+++ SecondJava/src/com/company/Main.java
@@ -1,9 +1,38 @@
 package com.company;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Main {
 //type psvm and hit tab the line below will appear
+//cntrl+D  for duplicating
     public static void main(String[] args) {
-        System.out.print("H");
-        System.out.println("ello Death!!");
+        String anyName = "Taylor";
+        System.out.println("Hello "+ anyName);
+        String UserInput = "entertainment";
+        System.out.println("Contains : "+UserInput.contains("enter"));
+
+        int counter=0;
+        String str1 = Integer.toString(counter);
+
+
+
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.white);
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setVisible(true);
+        JLabel welcome = new JLabel(str1);
+        for(int i=0;i<5;i++){
+            counter=i;
+            System.out.println(counter);
+            frame.getContentPane().add(welcome);
+        }
+        frame.setVisible(true);
     }
 }
