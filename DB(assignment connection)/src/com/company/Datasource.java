1a08ff75206f34237e82d6296931a0cfbd4aa606
sinarah22 <sinarahmannejad@gmail.com>
2019-12-09 11:30:51 -0800

    deleting Db works now!

--- DB(assignment connection)/src/com/company/Datasource.java
+++ DB(assignment connection)/src/com/company/Datasource.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,29 +16,75 @@ public class Datasource {
     private PreparedStatement queryProductCategoryInfo;
     private PreparedStatement queryProduct;
     private PreparedStatement queryCategory;
+    private PreparedStatement queryCustomer;
+    private PreparedStatement queryCard;
     private PreparedStatement insertIntoProducts;
     private PreparedStatement insertIntoCatagories;
+    private PreparedStatement insertIntoCustomers;
     private PreparedStatement queryProductCategory;
     private PreparedStatement insertIntoProductCategories;
+    private PreparedStatement deleteProducts;
+    private PreparedStatement deleteProductsCategory;
 
 
     public boolean open() {
         try {
             conn = DriverManager.getConnection(CONNECTION_STRING + DATABASE_BUSINESS);
 
-            queryProduct = conn.prepareStatement("SELECT * FROM products WHERE name = ?");
-            queryCategory = conn.prepareStatement("SELECT * FROM categories WHERE name = ?");
+
+
+            StringBuilder productcategoryquery = new StringBuilder();
+            productcategoryquery.append("SELECT p.name AS Product,  p.price AS Price, c.name AS Category ");
+            productcategoryquery.append("FROM products p ");
+            productcategoryquery.append("JOIN products_categories pc ");
+            productcategoryquery.append("ON p.id = pc.products_id ");
+            productcategoryquery.append("JOIN categories c ");
+            productcategoryquery.append("ON c.id = pc.categories_id ");
+            productcategoryquery.append("JOIN currencies cur ");
+            productcategoryquery.append("ON p.currency_id = cur.id ");
+            productcategoryquery.append("where p.active = 1 ");
+            productcategoryquery.append("ORDER BY p.name");
+            queryProductCategoryInfo = conn.prepareStatement(productcategoryquery.toString());
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("SELECT p.name AS Product,  p.price AS Price, c.name AS Category, ");
+            sb.append("(SELECT curp.value FROM currencies_price curp WHERE id = 2) * p.price AS PriceUSD, ");
+            sb.append("(SELECT curp.value FROM currencies_price curp WHERE id = 4) * p.price AS PriceJPY ");
+            sb.append("FROM products p ");
+            sb.append("JOIN products_categories pc ");
+            sb.append("ON p.id = pc.products_id ");
+            sb.append("JOIN categories c ");
+            sb.append("ON c.id = pc.categories_id ");
+            sb.append("JOIN currencies cur ");
+            sb.append("ON p.currency_id = cur.id ");
+            sb.append("JOIN currencies_price curp ");
+            sb.append("ON curp.currency_id  = cur.id ");
+            sb.append("where p.active = 1 ");
+            sb.append("ORDER BY p.name");
+            queryProductCategoryCurrenciesInfo = conn.prepareStatement(sb.toString());
+
+            queryProduct = conn.prepareStatement("SELECT * FROM products WHERE name = ? AND active = 1");
+            queryProductsInfo = conn.prepareStatement("SELECT * FROM products WHERE active = 1");
+            queryCard = conn.prepareStatement("SELECT * FROM cards WHERE active =1");
+            queryCategory = conn.prepareStatement("SELECT * FROM categories WHERE name = ? AND active = 1");
+            queryCategoryInfo = conn.prepareStatement("SELECT * FROM categories WHERE active =1");
+            queryCustomersInfo = conn.prepareStatement("SELECT * FROM customers WHERE active =1");
             insertIntoProducts = conn.prepareStatement("INSERT INTO products (name, description, price, currency_id, created_at) VALUES(?, ? , ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             insertIntoCatagories = conn.prepareStatement("INSERT INTO categories (name, sub_category_id, created_at) VALUES(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-            queryProductCategory = conn.prepareStatement("SELECT * FROM products_categories WHERE products_id = ? AND categories_id = ?");
+            queryProductCategory = conn.prepareStatement("SELECT * FROM products_categories WHERE products_id = ? AND categories_id = ? AND active =1");
+            queryCustomer = conn.prepareStatement("SELECT * FROM customers WHERE email = ? AND active =1", Statement.RETURN_GENERATED_KEYS);
+            queryCard = conn.prepareStatement("SELECT * FROM cards WHERE active =1");
             insertIntoProductCategories = conn.prepareStatement("INSERT INTO products_categories (products_id, categories_id, created_at) VALUES (?, ?, ?)");
+            insertIntoCustomers = conn.prepareStatement("INSERT INTO customers (name, email, address, phone, created_at) VALUES (? , ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            deleteProducts = conn.prepareStatement("UPDATE products SET active = 0 WHERE id  = ?  ");
+            deleteProductsCategory = conn.prepareStatement("UPDATE products_categories SET active = 0 WHERE products_id  = ? ");
             return true;
         } catch (SQLException e) {
             System.out.println("Couldn't connect to database: " + e.getMessage());
             return false;
         }
     }
-
 
     public void close() {
         try {
@@ -52,6 +97,7 @@ public class Datasource {
             if (queryCategoryInfo != null) {
                 queryCategoryInfo.close();
             }
+
 
             if (queryProductCategoryCurrenciesInfo != null) {
                 queryProductCategoryCurrenciesInfo.close();
@@ -67,6 +113,11 @@ public class Datasource {
             if (queryProduct != null) {
                 queryProduct.close();
             }
+
+            if(queryCard != null){
+                queryCard.close();
+            }
+
             if (insertIntoCatagories != null) {
                 insertIntoCatagories.close();
             }
@@ -87,7 +138,7 @@ public class Datasource {
 
         List<Product> pl = new ArrayList<>();
         try {
-            queryProductsInfo = conn.prepareStatement("SELECT * FROM products");
+
             ResultSet rs = queryProductsInfo.executeQuery();
 
             while (rs.next()) {
@@ -115,7 +166,6 @@ public class Datasource {
 
         List<Category> cl = new ArrayList<>();
         try {
-            queryCategoryInfo = conn.prepareStatement("SELECT * FROM categories");
             ResultSet rs = queryCategoryInfo.executeQuery();
 
             while (rs.next()) {
@@ -142,7 +192,6 @@ public class Datasource {
         List<Customer> cl = new ArrayList<>();
 
         try {
-            queryCustomersInfo = conn.prepareStatement("SELECT * FROM customers");
             ResultSet rs = queryCustomersInfo.executeQuery();
 
             while (rs.next()) {
@@ -169,18 +218,6 @@ public class Datasource {
     public void queryProductCategory() {
 
         try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("SELECT p.name AS Product,  p.price AS Price, c.name AS Category ");
-            sb.append("FROM products p ");
-            sb.append("JOIN products_categories pc ");
-            sb.append("ON p.id = pc.products_id ");
-            sb.append("JOIN categories c ");
-            sb.append("ON c.id = pc.categories_id ");
-            sb.append("JOIN currencies cur ");
-            sb.append("ON p.currency_id = cur.id ");
-            sb.append("where p.active = 1 ");
-            sb.append("ORDER BY p.name");
-            queryProductCategoryInfo = conn.prepareStatement(sb.toString());
 
             ResultSet rs = queryProductCategoryInfo.executeQuery();
             System.out.println("________________________________________");
@@ -207,22 +244,7 @@ public class Datasource {
     public void queryProductCategoryCurrencies() {
 
         try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("SELECT p.name AS Product,  p.price AS Price, c.name AS Category, ");
-            sb.append("(SELECT curp.value FROM currencies_price curp WHERE id = 2) * p.price AS PriceUSD, ");
-            sb.append("(SELECT curp.value FROM currencies_price curp WHERE id = 4) * p.price AS PriceJPY ");
-            sb.append("FROM products p ");
-            sb.append("JOIN products_categories pc ");
-            sb.append("ON p.id = pc.products_id ");
-            sb.append("JOIN categories c ");
-            sb.append("ON c.id = pc.categories_id ");
-            sb.append("JOIN currencies cur ");
-            sb.append("ON p.currency_id = cur.id ");
-            sb.append("JOIN currencies_price curp ");
-            sb.append("ON curp.currency_id  = cur.id ");
-            sb.append("where p.active = 1 ");
-            sb.append("ORDER BY p.name");
-            queryProductCategoryCurrenciesInfo = conn.prepareStatement(sb.toString());
+
 
             ResultSet rs = queryProductCategoryCurrenciesInfo.executeQuery();
             System.out.println("_____________________________________________________");
@@ -367,15 +389,66 @@ public class Datasource {
 
     }
 
+    //inserting customer should insert card automaticaly
+
+    //change this method to a transaction
+    public int insertCustomer(String name, String email, String address, String phone ) throws SQLException{
+
+        queryCustomer.setString(1, email);
+        ResultSet results = queryCustomer.executeQuery();
+
+        if (results.next()) {
+
+            return results.getInt("id");
+
+        } else {
+
+            insertIntoCustomers.setString(1, name);
+            insertIntoCustomers.setString(2, email);
+            insertIntoCustomers.setString(3, address);
+            insertIntoCustomers.setString(4, phone);
+            insertIntoCustomers.setString(5, new Timestamp(currentTimeMillis()).toString());
+
+            //this returns integer
+            //execute will return boolean
+            int affectedRows = insertIntoCustomers.executeUpdate();
+
+            if (affectedRows != 1) {
+                throw new SQLException("Couldn't insert customer!");
+            }
+
+            ResultSet generatedKeys = insertIntoCustomers.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return generatedKeys.getInt(1);
+            } else {
+                throw new SQLException("Couldn't get id for customer");
+            }
+        }
+
+    }
+
+    public void deletingProducts (String name) throws SQLException{
+        queryProduct.setString(1, name);
+        ResultSet results = queryProduct.executeQuery();
+
+        if (results.next()) {
+
+            int products_id = results.getInt("id");
+            deleteProducts.setInt(1,products_id);
+            deleteProductsCategory.setInt(1,products_id);
+            int affectedRows1 = deleteProducts.executeUpdate();
+            int affectedRows2 = deleteProductsCategory.executeUpdate();
+
+            if (affectedRows1 != 1 || affectedRows2 != 1) {
+                throw new SQLException("Couldn't Delete Product!");
+            }
 
 
+        } else {
 
-
-
-
-
-
-
+            System.out.println("Couldn't get id for Product");
+        }
+    }
 
 
 
--- DB(assignment connection)/src/com/company/Main.java
+++ DB(assignment connection)/src/com/company/Main.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import static java.lang.System.currentTimeMillis;
+import static java.lang.System.setOut;
 
 public class Main {
 
     public static void main(String[] args) {
-//too show the database in compiler
-        /*
+
         Datasource ds = new Datasource();
 
         if (ds.open()) {
@@ -25,65 +25,116 @@ public class Main {
             System.out.println("not connected to db");
         }
 
-        List<Product> productList = new ArrayList<>();
-        productList = ds.queryProducts();
+        int b = 1;
+        while (b==1){
+            System.out.println("\u001B[32m"+"c to Insert into Database \nr to Show Database \nu to Update  \nd to Delete \ne to Exit "+"\u001B[37m");
+            Scanner myObj = new Scanner(System.in);
+            String Menu = myObj.nextLine();
+            switch (Menu){
+                case "c":
+                    System.out.println("\n\nInserting ");
+                    System.out.println("[1-cusmtomer \t2-product and category ");
+                    Scanner myObj2 = new Scanner(System.in);
+                    int InsertMenu = myObj2.nextInt();
+                    switch (InsertMenu){
 
-        for(Product product : productList){
-            System.out.println(product.toString());
-        }
+                    case 1:
 
-        List<Category> categoryList = new ArrayList<>();
-        categoryList = ds.queryCategory();
+                        try {
+                            System.out.println("Customer name");
+                            String name = myObj.nextLine();
+                            System.out.println("Customer email");
+                            String email = myObj.nextLine();
+                            System.out.println("Customer address");
+                            String address = myObj.nextLine();
+                            System.out.println("Customer phone");
+                            String phone = myObj.nextLine();
+                            ds.insertCustomer(name,email,address,phone);
 
-        for(Category category : categoryList){
-            System.out.println(category.toString());
-        }
+                        }catch (SQLException e){
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                    case 2:
 
-        ds.queryProductCategory();
-*/
-        //***************************************************************************************************
-        Datasource ds = new Datasource();
+                          ds.queryProductCategoryCurrencies();
 
-        if (ds.open()) {
-            System.out.println("connected to db");
-        } else {
-            System.out.println("not connected to db");
-        }
+                          try {
 
-        List<Product> productList = new ArrayList<>();
-        productList = ds.queryProducts();
+                              ds.insertProductCategory("Cookie",
+                                      "cooked with chocolate and mapple syrup",
+                                      2.25,
+                                      1,
+                                      "Snack",
+                                      5);
 
-        for(Product product : productList){
-            System.out.println(product.toString());
-        }
+                          } catch (SQLException e){
+                              System.out.println(e.getMessage());
+                          }
 
-        List<Category> categoryList = new ArrayList<>();
-        categoryList = ds.queryCategory();
 
-        for(Category category : categoryList){
-            System.out.println(category.toString());
-        }
 
-        ds.queryProductCategoryCurrencies();
 
-        try {
+                          break;
+                    }
+                case "r":
+                    System.out.println("\n\nReading from Database ");
+                    System.out.println("press :\n" +
+                            " 1 to see Products Info\n" +
+                            "2 to see Customers Info");
+                    Scanner myObj3 = new Scanner(System.in);
+                    int Menu2 = myObj.nextInt();
+                    if(Menu2==1){
+                    ds.queryProductCategoryCurrencies();}
+                    else if(Menu2== 2){
 
-            ds.insertProductCategory("Cookie",
-                    "cooked with chocolate and mapple srup",
-                    2.25,
-                    1,
-                    "Snack",
-                    5);
+                        List<Customer> customerList = new ArrayList<>();
+                        customerList = ds.queryCustomers();
 
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
+                          for(Customer i : customerList){
+                              System.out.println(i.toString());
+                          }
+                    }
+                    break;
+                case "u":
+                    System.out.println("\n\nUpdating  ");
+                    ds.queryProductCategoryCurrencies();
 
-        List<Customer> customerList = new ArrayList<>();
-        customerList = ds.queryCustomers();
+                    try {
 
-        for(Customer i : customerList){
-            System.out.println(i.toString());
+                        ds.insertProductCategory("Cookie",
+                                "cooked with chocolate and mapple syrup",
+                                2.25,
+                                1,
+                                "Snack",
+                                5);
+
+                    } catch (SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+
+
+
+
+                    break;
+                case "d":
+                    System.out.println("\n\nDelete ");
+                    String Deleter = myObj.nextLine();
+                    System.out.println(Deleter);
+                    try{
+                    ds.deletingProducts(Deleter);
+                    } catch (SQLException e){
+                            System.out.println(e.getMessage());
+                        }
+
+                    break;
+                case "e":
+                    b=0;
+                    break;
+                default:
+                    System.out.println("\n\nNot in the menu");
+                    break;
+            }
         }
 
         ds.close();
